9b5d7e1f7b0e13855487eb2aa965ffdb7b421c60
minel1410 <msalihagic@hotmail.com>
2024-09-14 12:33:43 +0200

    Make Footer finalised for deployment

--- app/src/app/(sections)/Footer.jsx
+++ app/src/app/(sections)/Footer.jsx
@@ -7,11 +7,42 @@ const Footer = () => {
       <footer className="bg-Navy">
         <div className="w-full flex justify-center pt-16 pb-4">
           <div className="flex gap-8">
-            <FeatherIcon icon="github" className="text-Slate"></FeatherIcon>
-            <FeatherIcon icon="instagram" className="text-Slate"></FeatherIcon>
-            <FeatherIcon icon="twitter" className="text-Slate"></FeatherIcon>
-            <FeatherIcon icon="linkedin" className="text-Slate"></FeatherIcon>
-            <FeatherIcon icon="codepen" className="text-Slate"></FeatherIcon>
+            <Link
+              href="https://github.com/minel1410"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="GitHub Profile"
+              className="w-6 h-6 stroke-current transition-all duration-300 ease-in-out transform hover:translate-y-[-5px] hover:cursor-pointer"
+            >
+              <FeatherIcon
+                icon="github"
+                className="text-Slate hover:text-Green"
+              ></FeatherIcon>
+            </Link>
+            <Link
+              href="https://instagram.com/minel.salihagic"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="GitHub Profile"
+              className="w-6 h-6 stroke-current transition-all duration-300 ease-in-out transform hover:translate-y-[-5px] hover:cursor-pointer hover:text-Green"
+            >
+              <FeatherIcon
+                icon="instagram"
+                className="text-Slate hover:text-Green"
+              ></FeatherIcon>
+            </Link>
+            <Link
+              href="https://linkedin.com/in/minel-salihagi%C4%87-9ba401300/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="GitHub Profile"
+              className="w-6 h-6 stroke-current transition-all duration-300 ease-in-out transform hover:translate-y-[-5px] hover:cursor-pointer hover:text-Green"
+            >
+              <FeatherIcon
+                icon="linkedin"
+                className="text-Slate hover:text-Green"
+              ></FeatherIcon>
+            </Link>
           </div>
         </div>
 
@@ -19,21 +50,12 @@ const Footer = () => {
           <p>
             Built by{" "}
             <Link
-              href={"#"}
+              href="https://minelsalihagic.com"
               className="relative text-Green after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-[0.5px] after:bg-Green after:transition-all after:duration-300 hover:after:w-full"
             >
               Minel Salihagić
             </Link>
           </p>
-          {/* <p>
-            Designed borrowed from{" "}
-            <Link
-              href={"#"}
-              className="relative text-Green after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-[0.5px] after:bg-Green after:transition-all after:duration-300 hover:after:w-full"
-            >
-              Brittany Chaing
-            </Link>
-          </p> */}
         </div>
       </footer>
     );
