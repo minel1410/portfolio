c043b9e37a3b11b308e224d08efd7b1bd4ffe631
minel1410 <msalihagic@hotmail.com>
2025-01-02 22:00:58 +0100

    Update minor things in the look and add new projects

--- app/src/app/hexatest/page.js
+++
@@ -1,9 +0,0 @@
-import Hexagon from "../components/Hexagon";
-
-export default function Test() {
-  return (
-    <div className="w-full h-screen bg-Navy flex items-center justify-center">
-      <Hexagon classname="!left-0 bg-red-500"></Hexagon>
-    </div>
-  );
-}
